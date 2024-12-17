169700cb6f0c59cf4cb928eb30ac56a42b0f78cf
Петров Максим Андреевич <m.petrov@univ.priv>
2024-12-17 10:33:44 +0300

    *change_first_cod*

--- 5l/1.py
+++ 5l/1.py
@@ -7,10 +7,11 @@ Radian=math.radians(float(N))
 cosinus = math.cos(Radian)
 
 sinus = 1-cosinus**2
-print(f"Cos(x) = {cosinus:.4f}")
 print(f"Sin(x) = {sinus:.4f}")
-if int(cosinus)==0:
-    print("tg(x) не существует")
-else:
-    tangens=float(sinus)/float(cosinus)
+print(f"Cos(x) = {cosinus:.4f}")
+
+if int(cosinus)!=0:
+    tangens = float(sinus) / float(cosinus)
     print(f"tg(x) = {tangens:.4f}")
+else:
+    print("tg(x) не существует")
